5a1907839281ef89c2f3ab7912297f610c5f3b98
Miguel Company <miggelvalen@hotmail.com>
2026-09-09 09:13:39 +0200

    test: Returns exception when city not found

--- tests/unit/test_weather.py
+++ tests/unit/test_weather.py
@@ -28,3 +28,16 @@ class TestWeather:
             response = self.client.get("/api/v1/weather?city=Valencia")
             assert response.status_code == 200
             mock_get_weather.assert_called_once_with("Valencia")
+
+    def test_get_weather_without_city_returns_422(self):
+        response = self.client.get("/api/v1/weather")
+        assert response.status_code == 422
+
+    def test_get_weather_for_invalid_city_returns_404(self):
+        with patch(
+            "src.api.v1.weather.weather.get_weather_for_city"
+        ) as mock_get_weather:
+            mock_get_weather.return_value = None
+            response = self.client.get("/api/v1/weather?city=InvalidCity")
+            assert response.status_code == 404
+            assert response.json() == {"detail": "City not found"}
